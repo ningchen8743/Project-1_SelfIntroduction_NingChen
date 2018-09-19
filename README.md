3d1969dc4528e235f611ec71f1fd33b3cce1128a
ningchen8743 <ning.chen.cathy@gmail.com>
2018-09-18 23:12:58 -0400

    Modified final version

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Project 1
 + By: Ning Chen
-+ Production URL: <http://p1.ningchenbunny.com/>
++ Production URL [link](http://p1.ningchenbunny.com/)
 
 ## Outside resources
-n/a
++ Emoji CSS [Link](https://afeld.github.io/emoji-css/)
++ w3schools: Clearfix [Link](https://www.w3schools.com/howto/howto_css_clearfix.asp)
++ w3schools: HTML Color Names [link](https://www.w3schools.com/colors/colors_names.asp)
++ designshack: The 10 Best Script and Handwritten Google Fonts [link](https://designshack.net/articles/css/the-10-best-script-and-handwritten-google-web-fonts/)
 
 ## Notes for instructor
 n/a
--- index.php
+++ index.php
@@ -1,23 +1,34 @@
 <?php require('p1-logic.php');?>
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
 	<title>Ning Chen's self-introduction</title>
 	<meta charset='utf-8'>
 	
 	<link rel="stylesheet" href='css/style.css'>
+	<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+
 </head>
+
 <body>
 	<h1>Ning Chen</h1>
-	<img src='/images/profile.JPG' alt='Profile Photo'>
 	
-	<h2>About Me</h2>
-	<p>My name is Ning Chen, I am originally from Beijing, China. After several years of study and working in the US since 2011, I am currently living in the upstate NY with my husband. In the past few years I have been working as a GIS analyst for Norfolk Southern Corp in Atlanta, GA.<br> 
-	I love to spend my spare time practicing harp on daily basis, and yoga+swimming are my choices of exercise. I studied urban design in college so field sketch has always been a hobby whenever I travel to somewhere.</p>
+	<div class="clearfix">
+	<img class='img' src='/images/profile.JPG' alt='Profile Photo'>
+	<h2 class="self_intro">About Me</h2>
+	<p>My name is Ning Chen, I am originally from Beijing, China. After several years of study and working in the US since 2011, I am currently living in the upstate NY with my husband. In the past few years I have been working as a GIS analyst for Norfolk Southern Corp in Atlanta, GA.<br><br>
+	I love to spend my spare time practicing harp on daily basis, and yoga+swimming are my choices of exercise. I studied urban design in college so field sketch has always been a hobby whenever I travel to somewhere.<br><br>
+	The profile image was drawn by my husband, the bunny playing the harp is me. <i class="em em-blush"></i>
+	</p>
+	</div>
+	<br>
+	<div class="quotes">
+	<h2 class="quotes">Quote</h2>
+	<p class="quote_font"><?php echo $quote_array[$random_key];?></p>
+	</div>
 	
-	<h2>Quote</h2>
-	<?php echo $quote_array[$random_key];?>
-
 </body>
 
 </html>
