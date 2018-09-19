3e79227c18bb13f0b9166cbacf2fd388bde9e8f2
ningchen8743 <ning.chen.cathy@gmail.com>
2018-09-18 23:40:08 -0400

    modified final version with more quote

--- p1-logic.php
+++ p1-logic.php
@@ -4,6 +4,7 @@ $quote_array = [
     'Strive not to be a success, but rather to be of value.  -Albert Einstein',
     'The best way to find yourself is to lose yourself in the service of others.  -Mahatma Gandhi',
     'Travel is fatal to prejudice, bigotry, and narrow-mindedness.  -Mark Twain',
+	'Have more than you show, speak less than you know.  -William Shakespeare, King Lear',
 ];
 
 $random_key = array_rand($quote_array, 1);
